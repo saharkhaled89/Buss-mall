5dfc1d99a814119e07b98676ff13e6f9b78e8703
sahar khaled <engineer.sahar.8989@hotmail.com>
2020-03-03 05:18:22 +0200

    add

--- js/app.js
+++ js/app.js
@@ -24,15 +24,16 @@ var pictursImages = [
 var leftPicturImage = document.querySelector('#left_pictur_img');
 var centerPicturImage = document.querySelector('#center_pictur_img');
 var rightPicturImage = document.querySelector('#right_pictur_img');
-var groupImageSection = document.getElementById('all_picture');
+// var groupImageSection = document.getElementById('all_picture');
 var picturs = [];//an array to store all picture objects
 var totalClicks = 1;
 var times_showed = [];
 var times_clicked = [];
-for(var i = 0 ; i < pictursImages.length; i++){
+var lastSeen=[];
+for (var i = 0; i < pictursImages.length; i++) {
   times_showed.push(0);
   times_clicked.push(0);
-  
+
 }
 console.log(times_showed);
 console.log(times_clicked);
@@ -47,9 +48,10 @@ console.log(times_clicked);
 // rightPicturImage.alt = pictursImages[2];
 
 
-function Pictur (name){
+function Pictur(name) {
   this.name = name.split('.')[0];
   this.urlImage = `img/${this.name}.jpg`;
+  this.likes=0;
   picturs.push(this);//this its refer to the object that im created
 }
 
@@ -59,25 +61,27 @@ var rightPicturImageAT;
 
 
 
-function pickRandomImages(){
-  var leftImageRandom =  picturs[randomNumber(0 , picturs.length-1 )];
-  var centerImageRandom =  picturs[randomNumber(0 , picturs.length-1 )];
-  var rightImageRandom = picturs[randomNumber(0 , picturs.length-1 )];
-  leftPicturImage.setAttribute('src' , leftImageRandom.urlImage);
-  leftPicturImage.setAttribute('alt' , leftImageRandom.name);
-  centerPicturImage.setAttribute('src' , centerImageRandom.urlImage);
-  centerPicturImage.setAttribute('alt' , centerImageRandom.name);
-  rightPicturImage.setAttribute('src' , rightImageRandom.urlImage);
-  rightPicturImage.setAttribute('alt' ,rightImageRandom.name);
-  leftPicturImageAT = leftPicturImage.getAttribute('alt');
-  centerPicturImageAT = centerPicturImage.getAttribute('alt');
-  rightPicturImageAT = rightPicturImage.getAttribute('alt');
+function pickRandomImages() {
+  var leftImageRandom = picturs[randomNumber(0, picturs.length - 1)];
+  var centerImageRandom = picturs[randomNumber(0, picturs.length - 1)];
+  var rightImageRandom = picturs[randomNumber(0, picturs.length - 1)];
+  leftPicturImage.setAttribute('src', leftImageRandom.urlImage);
+  leftPicturImage.setAttribute('alt', leftImageRandom.name);
+  centerPicturImage.setAttribute('src', centerImageRandom.urlImage);
+  centerPicturImage.setAttribute('alt', centerImageRandom.name);
+  rightPicturImage.setAttribute('src', rightImageRandom.urlImage);
+  rightPicturImage.setAttribute('alt', rightImageRandom.name);
+  leftPicturImageAT = leftImageRandom.name;
+  centerPicturImageAT = centerImageRandom.name;
+  rightPicturImageAT = rightImageRandom.name;
 
   //console.log(leftPicturImageAT);
-  if(leftPicturImageAT === rightPicturImageAT || leftPicturImageAT === centerPicturImageAT || centerPicturImageAT === rightPicturImageAT || centerPicturImageAT === leftPicturImageAT ){
-    while(leftPicturImageAT === rightPicturImageAT || leftPicturImageAT === centerPicturImageAT || centerPicturImageAT === rightPicturImageAT || centerPicturImageAT === leftPicturImageAT ){
-      //pick another random number
+
+  if (lastSeen.includes(leftPicturImageAT) || lastSeen.includes(centerPicturImageAT)  ||  lastSeen.includes(rightPicturImageAT) || leftPicturImageAT === rightPicturImageAT || leftPicturImageAT === centerPicturImageAT || centerPicturImageAT === rightPicturImageAT || centerPicturImageAT === leftPicturImageAT) {
+    while (lastSeen.includes(leftPicturImageAT) || lastSeen.includes(centerPicturImageAT)  ||  lastSeen.includes(rightPicturImageAT)  || leftPicturImageAT === rightPicturImageAT || leftPicturImageAT === centerPicturImageAT || centerPicturImageAT === rightPicturImageAT || centerPicturImageAT === leftPicturImageAT) {
+
       pickRandomImages();
+
     }
   }
   else {
@@ -87,11 +91,13 @@ function pickRandomImages(){
     times_showed[pictursImages.indexOf(left_title)] += 1;
     times_showed[pictursImages.indexOf(center_title)] += 1;
     times_showed[pictursImages.indexOf(right_title)] += 1;
-    
-  } 
+
+
+  }
+  lastSeen = [];
 }
 
-for(var i = 0; i< pictursImages.length ; i++){
+for (var i = 0; i < pictursImages.length; i++) {
   new Pictur(pictursImages[i]);//we pass the name of the picturs from the array
 }
 pickRandomImages();
@@ -103,36 +109,50 @@ console.log(picturs);
 
 // Variables to store the picturs already on the page
 // the allImages array is a property of the Picture constructor
-function clickImage(e){
-  if( e.target.id === 'left_pictur_img' || e.target.id ==='center_pictur_img'|| e.target.id === 'right_pictur_img'){
-    if(e.target.id === 'left_pictur_img'){
+function clickImage(e) {
+  if (e.target.id === 'left_pictur_img' || e.target.id === 'center_pictur_img' || e.target.id === 'right_pictur_img') {
+    if (e.target.id === 'left_pictur_img') {
       var left_title = leftPicturImage.getAttribute('alt');
       times_clicked[pictursImages.indexOf(left_title)] += 1;
     }
-    if(e.target.id === 'center_pictur_img'){
+    if (e.target.id === 'center_pictur_img') {
       var center_title = centerPicturImage.getAttribute('alt');
       times_clicked[pictursImages.indexOf(center_title)] += 1;
     }
-    if(e.target.id === 'right_pictur_img'){
-      var right_title = centerPicturImage.getAttribute('alt');
+    if (e.target.id === 'right_pictur_img') {
+      var right_title = rightPicturImage.getAttribute('alt');
       times_clicked[pictursImages.indexOf(right_title)] += 1;
     }
+    lastSeen=[];
+    lastSeen.push(leftPicturImage.getAttribute('alt'));
+    lastSeen.push(centerPicturImage.getAttribute('alt'));
+    lastSeen.push(rightPicturImage.getAttribute('alt'));
+    
+
     pickRandomImages();
     totalClicks++;
+
   }
-  if(totalClicks === 26){
+  if (totalClicks === 26) {
     //remove event listener
     leftPicturImage.remove();
     rightPicturImage.remove();
     centerPicturImage.remove();
     console.log('finished');
     var report = document.querySelector('#report');
-    for(var i = 0; i <pictursImages.length; i++){
-      var x = document.createElement("LI");
-      var t = document.createTextNode(pictursImages[i]+" was shown " + times_showed[i]+" times and had votes "+ times_clicked[i]+" times.");
+    for (var i = 0; i < pictursImages.length; i++) {
+      var x = document.createElement('LI');
+      var t = document.createTextNode(pictursImages[i] + ' was shown ' + times_showed[i] + ' times and had votes ' + times_clicked[i] + ' times.');
       x.appendChild(t);
       report.appendChild(x);
+
+
+
+
+
+
     }
+    renderChartResult();
 
   }
 }
@@ -141,6 +161,7 @@ function clickImage(e){
 rightPicturImage.addEventListener('click', clickImage);
 leftPicturImage.addEventListener('click', clickImage);
 centerPicturImage.addEventListener('click', clickImage);
+
 
 // groupImageSection.addEventListener('click' , clickImage);
 
@@ -153,6 +174,86 @@ centerPicturImage.addEventListener('click', clickImage);
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+
+
+
+function renderChartResult(){
+
+  var pictursNames = [];
+
+  for(var i = 0 ; i < picturs.length; i++){
+    var picturName = picturs[i].name;
+    pictursNames.push(picturName);
+
+
+  }
+
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: pictursNames,
+      datasets: [{
+        label: '# of show',
+
+        data: times_showed,
+
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        backgroundColor2: 'rgb(0,0,205)',
+
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderColor2: 'rgb(0,0,205)',
+        borderWidth: 1
+
+
+      },
+
+      {
+        label: '# of votes',
+        data: times_clicked,
+        backgroundColor: 'rgb(0, 191, 255)',
+        borderColor: 'rgba(295, 99, 132, 1)',
+        borderWidth: 1,
+      } ]
+
+
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
